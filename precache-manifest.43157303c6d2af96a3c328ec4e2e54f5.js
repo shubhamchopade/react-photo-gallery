525a8ad09d05a175f53644e8596a9b634b83f4bd
Shubham Chopade <shubhamchopade10@gmail.com>
2020-08-15 21:04:11 +0530

    Updates

---
+++ precache-manifest.43157303c6d2af96a3c328ec4e2e54f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74054ee2b97c8004be981bd4be8fa91c",
+    "url": "/react-photo-gallery/index.html"
+  },
+  {
+    "revision": "0fe27e5e8eac56effdf0",
+    "url": "/react-photo-gallery/static/css/main.1d66e41c.chunk.css"
+  },
+  {
+    "revision": "29ae282ed3534b6367db",
+    "url": "/react-photo-gallery/static/js/2.9261d6e5.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-photo-gallery/static/js/2.9261d6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe27e5e8eac56effdf0",
+    "url": "/react-photo-gallery/static/js/main.f6acff9a.chunk.js"
+  },
+  {
+    "revision": "02a4296d5256a6523520",
+    "url": "/react-photo-gallery/static/js/runtime-main.24cbca52.js"
+  }
+]);
